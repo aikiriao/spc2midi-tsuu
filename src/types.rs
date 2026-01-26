2e1d4e4bfe89cd9d217b17f958247c99b21c79c6
aikiriao <samuraiaiki@gmail.com>
2026-01-26 21:37:27 +0900

    increase default clock-up factor.

--- src/types.rs
+++ src/types.rs
@@ -13,7 +13,7 @@ pub const DEFAULT_MIDI_BPM: f32 = 120.0;
 /// デフォルトの出力MIDIの四分音符内のティック数
 pub const DEFAULT_MIDI_RESOLUSIONS: u16 = 480;
 /// デフォルトのSPCのクロックアップ倍率
-pub const DEFAULT_SPC_CLOCKUP_FACTOR: u32 = 8;
+pub const DEFAULT_SPC_CLOCKUP_FACTOR: u32 = 16;
 /// 最小のBPM（テンポ）
 pub const MIN_BEATS_PER_MINUTE: u32 = 30;
 /// 最大のBPM（テンポ）
